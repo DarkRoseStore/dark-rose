ab02068617686bf5e441bd51b49140daa0735ff3
DarkRoseStore <darkRose2352@gmail.com>
2025-01-18 19:35:04 -0300

    Update Hero.tsx

--- src/pages/Hero/Hero.tsx
+++ src/pages/Hero/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="heroContainer">
       <div className="header">
-        <p className="text-lg">+55 12 3456-7890</p>
+        <p className="text-lg">CNPJ: 58.779.070/0001-70</p>
         <h1 className="headerTitle">Dark Rose</h1>
         <hr className="absolute w-[95vw]" />
       </div>
