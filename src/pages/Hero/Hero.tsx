b7a6e84aa00f9433d0eb19a61bb714f7b8bb1bd1
DarkRoseStore <darkRose2352@gmail.com>
2025-01-24 21:26:25 -0300

    Add files via upload

--- src/pages/Hero/Hero.tsx
+++ src/pages/Hero/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="heroContainer">
       <div className="header">
-        <h1 className="headerTitle">Minhocas Voadoras – Serviços Literários</h1>
+        <h1 className="headerTitle">Minhocas Voadoras</h1>
         <h2 className="headerSubtitle">Dando asas às suas palavras, raízes aos seus sonhos!</h2>
         <hr className="absolute w-[95vw]" />
       </div>
